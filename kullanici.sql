c3a8741a04d5b8b467adac5bb6ac5431e1e8a73a
denizozdemi <denizozdemir.1@outlook.com>
2024-07-21 13:13:02 +0300

    Add files via upload
    Login sayfası için sql bilgileri

---
+++ kullanici.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jul 21, 2024 at 10:10 AM
+-- Server version: 8.0.17
+-- PHP Version: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `eticaret1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kullanici`
+--
+
+CREATE TABLE `kullanici` (
+  `kullanici_id` int(11) NOT NULL,
+  `kullanici_zaman` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `kullanici_resim` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_tc` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_ad` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_mail` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_gsm` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_password` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_adsoyad` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_adres` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_il` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_ilce` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_unvan` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_yetki` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_durum` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kullanici`
+--
+ALTER TABLE `kullanici`
+  ADD PRIMARY KEY (`kullanici_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
